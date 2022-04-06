3c1c10ac90dff0af1f8f24f39b6bed2e96e70110
Grzegorz Gozdziewski <grzegorz.gozdziewski@ppuc.pl>
2022-04-06 20:11:56 +0200

    Remove the delete recipe button for the user

--- src/app/recipes-list/details/details.component.ts
+++ src/app/recipes-list/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 import { Recipes, RecipesApiService } from 'src/app/recipes-api.service';
 
 @Component({
@@ -11,14 +12,22 @@ export class DetailsComponent implements OnInit {
   recipe!: Recipes;
   id!: number;
   error: string | null = null;
+  isAuthor!: boolean;
 
   constructor(
     private recipesApiService: RecipesApiService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.isAuthor = user?.role === 'author' ? true : false;
+      }
+    });
+
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.recipesApiService.getRecipe(this.id).subscribe(
